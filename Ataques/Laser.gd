f8d4aeb275fee4e028afffb2a65a6b1e4a10e29d
fergiaco <fernando.vgiacomini@gmail.com>
2023-05-08 16:36:05 -0300

    Bordas raio laser bidirecional

---
+++ Ataques/Laser.gd
@@ -0,0 +1,10 @@
+extends KinematicBody2D
+
+var velocidade=Vector2(1,0)
+var speed=900
+
+func _physics_process(delta):
+	var colisao = move_and_collide(velocidade.normalized()*delta*speed)
+	if colisao:
+		print('Laser colidiu')
+		self.queue_free()
--- Player/Bordas.gd
+++ Player/Bordas.gd
@@ -5,9 +5,10 @@ export (int) var jump_speed = 1000
 export (int) var gravity = 3000
 #export (PackedScene) var box : PackedScene
 
-onready var target = position # mesmo que func _ready() ...
+onready var target = position 
 onready var sprite = $Sprite
-onready var box := preload("res://Cenario/Plataforma.tscn")
+onready var Laser := preload("res://Ataques/Laser.tscn")
+onready var direcao = Vector2(1,0)
 
 var velocity = Vector2.ZERO
 var rotation_dir = 0
@@ -15,10 +16,17 @@ var rotation_dir = 0
 func get_side_input():
 	velocity.x = Input.get_action_strength("Right") - Input.get_action_strength("Left")	
 	velocity.x *=  speed
+	
 	if velocity.x > 0:
 		sprite.play("Direita")
+		#if direcao!=Vector2(1,0):
+		direcao=Vector2(1,0)
+		
 	elif velocity.x < 0:
 		sprite.play("Esquerda")
+		#if direcao!=Vector2(-1,0):
+		direcao=Vector2(-1,0)
+		
 	else:
 		sprite.stop()
 		sprite.frame = 0
@@ -27,12 +35,17 @@ func get_side_input():
 		velocity.y = -jump_speed
 		get_tree().call_group("HUD", "updateScore")
 
-	if Input.is_key_pressed(KEY_X):
-		var b = box.instance()
-		b.position = global_position
-		print(global_position)
-		owner.add_child(b)
+	if Input.is_action_pressed("Projetil") and pode_atirar():
+		var l = Laser.instance()
+		l.position = $Posicao_olhos.global_position
+		l.velocidade=direcao
+		l.add_collision_exception_with(get_node("."))
+		owner.add_child(l)
 		
+
+func pode_atirar():
+	#Implementar timer
+	return true
 	
 func _physics_process(delta):
 	get_side_input()	
